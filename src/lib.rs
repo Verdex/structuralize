cddcc3f0097fd6a6582abeec2fe6726c9f7894c4
David L. Inman <davidlevere@gmail.com>
2023-05-14 22:49:44 -0400

    todos

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 
 mod data;
 
+/* TODO Data: 
+                * enum
+                * From defs ?
+                * parser
+
+   TODO parser: 
+                * parser
+                * fn impl
+                * macro_rules ?
+    
+   TODO pattern matching:
+                * parser
+                * fn impl
+                * macro_rules ?
+*/
+
 #[cfg(test)]
 mod tests {
     use super::*;
