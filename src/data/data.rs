73cb8d95df1682624262a29464ac310fe68402c9
David L. Inman <davidlevere@gmail.com>
2023-05-14 23:09:09 -0400

    inc prog

--- src/data/data.rs
+++ src/data/data.rs
@@ -1,13 +1,20 @@
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Number {
     Float64(f64),
     Usize(usize),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Data {
     Number(Number),
-    Blarg(Box<str>),
-
+    String(String),
+    Symbol(String),
+    Cons { name: String, params: Vec<Data> },
+    Struct { name: String, fields: Vec<(String, Data)> },
+    List(Vec<Data>),
 }
+
+// TODO: macro that lets you use struct dot notation to access fields from struct item
+// TODO: derives needed so that Data can be used in hash?
+// TODO: Hash case for Data?
