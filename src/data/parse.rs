8b68cfb01ec57860397b0e0c21c5f4dba4c3081a
David L. Inman <davidlevere@gmail.com>
2023-05-21 00:29:03 -0400

    parse_list! moved to parsing

--- src/data/parse.rs
+++ src/data/parse.rs
@@ -5,43 +5,6 @@ use renounce::*;
 use crate::parsing::*;
 use super::data::*;
 
-macro_rules! parse_list {
-    ($input:ident => $l_bracket:ident, $target:ident : $target_type:ty, $r_bracket:ident) => {
-        {
-            pat!(parse_comma: char => () = ',' => ());
-
-            fn parse_target_comma<'a>(input : &mut Chars<'a>) -> Result<$target_type, ParseError> {
-                parser!(input => {
-                    _clear_0 <= parse_whitespace;
-                    target <= $target;
-                    _clear_1 <= parse_whitespace;
-                    _comma <= parse_comma;
-                    _clear_2 <= parse_whitespace;
-                    select target 
-                })
-            }
-
-            parser!($input => {
-                _clear_0 <= parse_whitespace;
-                _left_bracket <= $l_bracket;
-                targets <= * parse_target_comma;
-                last_target <= ? $target;
-                _clear_1 <= parse_whitespace;
-                _right_bracket <= ! $r_bracket;
-                _clear_2 <= parse_whitespace;
-                select {
-                    let mut targets = targets;
-                    match last_target {
-                        Some(target) => { targets.push(target); },
-                        None => (),
-                    }
-                    targets
-                }
-            })
-        }
-    };
-}
-
 impl std::str::FromStr for Data {
     type Err = Box<dyn std::error::Error>;  
 
