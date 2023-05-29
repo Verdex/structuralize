0ed11c58c016915232f4d39607b0714a726608d6
David L. Inman <davidlevere@gmail.com>
2023-05-29 11:02:39 -0400

    inc prog for matcher

--- src/pattern/data.rs
+++ src/pattern/data.rs
@@ -1,9 +1,39 @@
 
+use std::hash::Hash;
+use std::collections::HashMap;
+
+use crate::data::*;
+
+
 #[derive(Debug, Clone)]
 pub enum Pattern {
     CaptureVar(String),
     As(String, Box<Pattern>),
     Cons { name: String, params: Vec<Pattern> },
+}
+
+#[derive(Debug)]
+pub struct MatchResult {
+    map : HashMap<Slot, Data>
+}
+
+impl<const N : usize> From<[(&String, &Data); N]> for MatchResult {
+    fn from(item : [(&String, &Data); N]) -> Self {
+        let map = item.into_iter().map(|(k,v)| (k.into(), v.clone())).collect::<HashMap<Slot, Data>>();
+        MatchResult { map }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub enum Slot {
+    Symbol(String),
+    Path(Vec<String>),
+}
+
+impl From<&String> for Slot {
+    fn from(item : &String) -> Self {
+        Slot::Symbol(item.to_string())
+    }
 }
 
 // TODO probably need comparison patterns in order to avoid needing expressions
--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -6,7 +6,14 @@ use crate::data::*;
 use super::data::*;
 
 // TODO:  it would be nice if the vec was some sort of iter
-pub fn pattern_match(data : Data, pattern : Pattern) -> Result<Vec<HashMap<String, Data>>, MatchError> {
+pub fn pattern_match(pattern : &Pattern, data : &Data) -> Result<Vec<MatchResult>, MatchError> {
+    use Pattern::*;
 
-    Ok(vec![])
+    match pattern {
+        CaptureVar(sym) => Ok(vec![ [(sym, data)].into() ]),
+        As(sym, pat) => {
+            todo!()
+        },
+        _ => todo!(),
+    }
 }
