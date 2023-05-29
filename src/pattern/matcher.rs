dcb038cad0538752aaab079b09472e887b10ccec
David L. Inman <davidlevere@gmail.com>
2023-05-29 14:56:18 -0400

    matcher inc prog; test

--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -6,16 +6,28 @@ use super::data::*;
 
 // TODO:  phantom type type checked patterns
 
-// TODO:  it would be nice if the vec was some sort of iter
 pub fn pattern_match(pattern : &Pattern, data : &Data) -> Vec<MatchResult> {
     use Pattern::*;
-
     match pattern {
-        CaptureVar(sym) => Ok(vec![ [(sym.into(), data)].into() ]),
-        As(sym, pat) => {
-            let 
-            todo!()
+        CaptureVar(s) => vec![[(s.into(), data)].into()],
+        As(s, p) => {
+            let x = pattern_match(p, data);
+            let as_result : MatchResult = [(s.into(), data)].into();
+            x.into_iter().map(|mut y| { y.merge(as_result.clone()); y }).collect()
         },
         _ => todo!(),
     }
+}
+
+#[cfg(test)] 
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_capture_var() {
+        let data = Data::Number(Number::Usize(0));
+        let results = pattern_match(&Pattern::CaptureVar("a".to_string()), &data);
+
+        assert_eq!(results.len(), 1);
+    } 
 }
