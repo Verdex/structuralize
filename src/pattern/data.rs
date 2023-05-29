e14ee58899c6069bd43c417fb868a74ac0ee1873
David L. Inman <davidlevere@gmail.com>
2023-05-29 10:02:21 -0400

    inc prog matcher

--- src/pattern/data.rs
+++ src/pattern/data.rs
@@ -8,3 +8,16 @@ pub enum Pattern {
 
 // TODO probably need comparison patterns in order to avoid needing expressions
     // This probably means that if-patterns aren't going to work
+
+#[derive(Debug)]
+pub enum MatchError {
+
+}
+
+impl std::fmt::Display for MatchError {
+   fn fmt(&self, f : &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "TODO")
+    }
+}
+
+impl std::error::Error for MatchError {}
---
+++ src/pattern/matcher.rs
@@ -0,0 +1,12 @@
+
+use std::collections::HashMap;
+
+use crate::data::*;
+
+use super::data::*;
+
+// TODO:  it would be nice if the vec was some sort of iter
+pub fn pattern_match(data : Data, pattern : Pattern) -> Result<Vec<HashMap<String, Data>>, MatchError> {
+
+    Ok(vec![])
+}
