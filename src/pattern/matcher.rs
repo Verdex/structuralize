350ff5b08a90e881fc8373b61950e1e0201f811d
David L. Inman <davidlevere@gmail.com>
2023-06-13 22:28:12 -0400

    inc prog for iterator of match results

--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -6,20 +6,28 @@ use super::data::*;
 
 // TODO:  phantom type type checked patterns
 
-pub struct MatchResults {
-
+pub struct MatchResults<'a, 'b> {
+    target : Vec<(&'a Pattern, &'b Data)>,
 }
 
-impl Iterator for MatchResults {
+impl<'a, 'b> Iterator for MatchResults<'a, 'b> {
     type Item = MatchResult;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let x = self.target.pop();
+
+        match x {
+            (Pattern::CaptureVar(name), data) => {
+
+            }
+        }
+
         None
     }
 }
 
-pub fn pattern_match(pattern : &Pattern, data : &Data) -> MatchResults {
-    MatchResults{}
+pub fn pattern_match<'a, 'b>(pattern : &'a Pattern, data : &'b Data) -> MatchResults<'a, 'b> {
+    MatchResults { target : vec![(pattern, data)]}
 }
 
 #[cfg(test)] 
