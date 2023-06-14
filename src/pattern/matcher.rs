de737979966cf3c5c7fd37d18df214d9eb7dab26
David L. Inman <davidlevere@gmail.com>
2023-06-13 21:09:44 -0400

    inc prog match results

--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -6,9 +6,20 @@ use super::data::*;
 
 // TODO:  phantom type type checked patterns
 
+pub struct MatchResults {
 
-pub fn pattern_match(pattern : &Pattern, data : &Data) -> MatchResult {
+}
 
+impl Iterator for MatchResults {
+    type Item = MatchResult;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+pub fn pattern_match(pattern : &Pattern, data : &Data) -> MatchResults {
+    MatchResults{}
 }
 
 #[cfg(test)] 
