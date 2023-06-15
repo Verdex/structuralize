bf822000392b4ebfef16ad6f7a98d9b970e35650
David L. Inman <davidlevere@gmail.com>
2023-06-14 23:27:32 -0400

    test matcher.rs: single var and cons with var tests

--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -34,4 +34,37 @@ pub fn pattern_match<'a, 'b>(pattern : &'a Pattern, data : &'b Data) -> MatchRes
 mod test {
     use super::*;
 
+    #[test]
+    fn should_match_single_var() {
+        let pattern = Pattern::CaptureVar("x".into());
+        let data : Data = "cons(a)".parse().unwrap();
+
+        let results = pattern_match(&pattern, &data).collect::<Vec<_>>();
+        assert_eq!(results.len(), 1);
+
+        let observed = results[0].get(&"x".into()).unwrap();
+        assert_eq!(observed, &data);
+    }
+
+    #[test]
+    fn should_match_cons_with_vars() {
+        let pattern = Pattern::Cons { name : "cons".into()
+                                    , params: vec![ Pattern::CaptureVar("x".into())
+                                                  , Pattern::CaptureVar("y".into())
+                                                  , Pattern::CaptureVar("z".into())
+                                                  ]
+                                   };
+        let data : Data = "cons(a, b, c)".parse().unwrap();
+
+        let results = pattern_match(&pattern, &data).collect::<Vec<_>>();
+        assert_eq!(results.len(), 1);
+
+        let observed_x = results[0].get(&"x".into()).unwrap();
+        let observed_y = results[0].get(&"y".into()).unwrap();
+        let observed_z = results[0].get(&"z".into()).unwrap();
+        assert_eq!(observed_x, &"a".parse::<Data>().unwrap());
+        assert_eq!(observed_y, &"b".parse::<Data>().unwrap());
+        assert_eq!(observed_z, &"c".parse::<Data>().unwrap());
+    }
+
 }
