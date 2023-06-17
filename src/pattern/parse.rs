b39129e51626e623e21c7c7491360307d4f5f39f
David L. Inman <davidlevere@gmail.com>
2023-06-17 14:07:24 -0400

    copy over parser for data but Data => Pattern

---
+++ src/pattern/parse.rs
@@ -0,0 +1,204 @@
+
+use std::str::Chars;
+use renounce::*;
+
+use crate::parsing::*;
+use super::data::*;
+
+impl std::str::FromStr for Pattern {
+    type Err = Box<dyn std::error::Error>;  
+
+    fn from_str(s : &str) -> Result<Self, Self::Err> {
+        // TODO:  the Chars struct will be returned at the point of failure
+        // in the event of a Fatal result.  Might be a good idea to somehow
+        // reflect that in a different concrete Error so that it can be shown
+        // to a consumer.
+        let mut x = s.chars();
+        let y = parse_pattern(&mut x);
+        println!("{}", x.collect::<String>());
+        Ok(y?)
+        //Ok(parse_data(&mut s.chars())?)
+    }
+}
+
+fn parse_pattern<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    fn options<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+        alt!(input => parse_data_float64; 
+                      parse_cons; 
+                      parse_struct;
+                      parse_list; 
+                      parse_symbol)
+    }
+
+    parser!(input => {
+        _before_clear <= parse_whitespace;
+        data <= options;
+        _after_clear <= parse_whitespace;
+        select data
+    })
+}
+
+fn parse_struct<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    pat!(parse_l_paren: char => () = '{' => ());
+    pat!(parse_r_paren: char => () = '}' => ());
+    pat!(parse_colon: char => () = ':' => ());
+
+    fn parse_field<'a>(input : &mut Chars<'a>) -> Result<(String, Pattern), ParseError> {
+        parser!(input => {
+            field_name <= parse_word;
+            _clear_1 <= parse_whitespace;
+            _colon <= ! parse_colon;
+            // Note: parse_data clears before and after itself
+            data <= parse_data; 
+            select (field_name, data)
+        })
+    }
+
+    fn parse_fields<'a>(input : &mut Chars<'a>) -> Result<Vec<(String, Pattern)>, ParseError> {
+        parse_list!(input => parse_l_paren, parse_field : (String, Pattern), parse_r_paren)
+    }
+
+    parser!(input => {
+        struct_name <= parse_word;
+        _clear <= parse_whitespace;
+        fields <= parse_fields;
+        select Pattern::Struct { name: struct_name, fields }
+    })
+}
+
+fn parse_cons<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    pat!(parse_l_paren: char => () = '(' => ());
+    pat!(parse_r_paren: char => () = ')' => ());
+
+    fn param_list<'a>(input : &mut Chars<'a>) -> Result<Vec<Pattern>, ParseError> {
+        parse_list!(input => parse_l_paren, parse_data : Pattern, parse_r_paren)
+    }
+
+    parser!(input => {
+        cons_name <= parse_word;
+        _clear <= parse_whitespace;
+        params <= param_list;
+        select Pattern::Cons { name: cons_name, params }
+    })
+}
+
+fn parse_symbol<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    parser!(input => {
+        word <= parse_word;
+        select Pattern::Symbol(word)
+    })
+}
+
+fn parse_data_float64<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    Ok(Pattern::Number(Number::Float64(parse_float64(input)?)))
+}
+
+fn parse_list<'a>(input : &mut Chars<'a>) -> Result<Pattern, ParseError> {
+    pat!(parse_l_square: char => () = '[' => ());
+    pat!(parse_r_square: char => () = ']' => ());
+
+    Ok(Pattern::List(parse_list!(input => parse_l_square, parse_data : Pattern, parse_r_square)?))
+}
+
+
+#[cfg(test)]
+mod test {
+    use intra::*;
+    use super::*;
+
+    fn slice<'a, T>(input : &'a Vec<T>) -> &'a [T] { &input[..] }
+    fn unbox<'a, T>(input : &'a Box<T> ) -> &'a T { &**input }
+
+    #[test]
+    fn should_parse_complex_data() {
+        let input = " name  { first : other { first : one( 1, 2, num([3, 2, 3, [blarg]]) ) } , second: inner ,  }";
+        let data = input.parse::<Pattern>().unwrap();
+        let mut matched = false;
+        atom!(data => [Pattern::Struct { .. }] => { 
+            matched = true;
+        } );
+        assert!(matched);
+    }
+
+    #[test]
+    fn should_parse_struct() {
+        let input = " name  { first : 1.0 , second: inner ,  }";
+        let data = input.parse::<Pattern>().unwrap();
+
+        let mut matched = false;
+        atom!(data => [Pattern::Struct { name, fields: ref fields}] fields; 
+                       slice $ [ [(first, Pattern::Number(Number::Float64(f))), (second, Pattern::Symbol(sym))] ] => { 
+            assert_eq!(name, "name");
+            assert_eq!(*f, 1f64);
+            assert_eq!(sym, "inner");
+            matched = true;
+        } );
+        assert!(matched);
+    }
+
+    #[test]
+    fn should_parse_cons() {
+        let input = " name  ( 1.0, inner, 5.5 )";
+        let data = input.parse::<Pattern>().unwrap();
+
+        let mut matched = false;
+        atom!(data => [Pattern::Cons { name, params: ref params }] params; 
+                       slice $ [ [Pattern::Number(Number::Float64(a)), Pattern::Symbol(_), Pattern::Number(Number::Float64(b))] ] => { 
+            assert_eq!(name, "name");
+            assert_eq!(*a, 1f64);
+            assert_eq!(*b, 5.5f64);
+            matched = true;
+        } );
+        assert!(matched);
+    }
+
+    #[test]
+    fn should_parse_symbol() {
+        let input = " symbol_123 ";
+        let data = input.parse::<Pattern>().unwrap();
+
+        let mut matched = false;
+        atom!(data => [Pattern::Symbol(sym)] => { 
+            assert_eq!(sym, "symbol_123");
+            matched = true;
+        } );
+        assert!(matched);
+    }
+
+    #[test]
+    fn should_parse_list() {
+        let input = " [ [], [1, 2], [1 , 2, 3], 4] ";
+        let data = input.parse::<Pattern>().unwrap();
+
+        let mut matched = false;
+        atom!(data => [Pattern::List(ref params)] params; 
+              slice $ [ [Pattern::List(first), Pattern::List(second), Pattern::List(third), Pattern::Number(Number::Float64(f))] ] => { 
+            assert_eq!(*f, 4f64);
+            assert_eq!(first.len(), 0);
+            assert_eq!(second.len(), 2);
+            assert_eq!(second[0], Pattern::Number(Number::Float64(1f64)));
+            assert_eq!(second[1], Pattern::Number(Number::Float64(2f64)));
+            assert_eq!(third.len(), 3);
+            assert_eq!(third[0], Pattern::Number(Number::Float64(1f64)));
+            assert_eq!(third[1], Pattern::Number(Number::Float64(2f64)));
+            assert_eq!(third[2], Pattern::Number(Number::Float64(3f64)));
+            matched = true;
+        } );
+
+        assert!(matched);
+    }
+
+    #[test]
+    fn should_parse_data_float64() {
+        let input = "-123.456E-2";
+        let data = input.parse::<Pattern>().unwrap();
+
+        let mut matched = false;
+        atom!(data => [Pattern::Number(Number::Float64(x))] => { 
+            assert_eq!(x, -123.456E-2);
+            matched = true;
+        } );
+
+        assert!(matched);
+    }
+}
