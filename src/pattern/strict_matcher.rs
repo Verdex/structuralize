cd4c6f9b62ef2611759c976ca2b037ac60feedb2
David L. Inman <davidlevere@gmail.com>
2023-07-30 23:10:27 -0400

    stub for strict pattern match

---
+++ src/pattern/strict_matcher.rs
@@ -0,0 +1,7 @@
+
+use crate::data::*;
+use super::data::*;
+
+pub fn strict_pattern_match<'pattern, 'data>(pattern : &'pattern Pattern, data : &'data Data) -> Vec<MatchResult<'data>> {
+    vec![]
+}
