7db86da00d1efc35459b8cc5214df5199718e5bc
David L. Inman <davidlevere@gmail.com>
2023-08-15 23:04:36 -0400

    remove todos

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,6 @@ mod parsing;
 pub mod data;
 pub mod pattern;
 
-/* 
-   TODO parser: 
-        * parser
-        * fn impl
-        * macro_rules ?
-    
-   TODO pattern matching:
-        * parser
-        * fn impl
-        * macro_rules ?
-*/
 
 #[cfg(test)]
 mod tests {
