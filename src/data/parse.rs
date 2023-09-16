219b465c340ba37d050398c6483475a80db2428f
David L. Inman <davidlevere@gmail.com>
2023-09-15 22:46:35 -0400

    remove <'a> from Chars

--- src/data/parse.rs
+++ src/data/parse.rs
@@ -32,7 +32,7 @@ impl std::str::FromStr for Data {
     }
 }
 
-fn parse<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse(input : &mut Chars) -> Result<Data, ParseError> {
     parser!(input => {
         data <= ! parse_data;
         ! end;
@@ -40,8 +40,8 @@ fn parse<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
     })
 }
 
-fn parse_data<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
-    fn options<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse_data(input : &mut Chars) -> Result<Data, ParseError> {
+    fn options(input : &mut Chars) -> Result<Data, ParseError> {
         alt!(input => parse_cons; 
                       parse_list; 
                       parse_symbol;
@@ -56,11 +56,11 @@ fn parse_data<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
     })
 }
 
-fn parse_cons<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse_cons(input : &mut Chars) -> Result<Data, ParseError> {
     pat!(parse_l_paren: char => () = '(' => ());
     pat!(parse_r_paren: char => () = ')' => ());
 
-    fn param_list<'a>(input : &mut Chars<'a>) -> Result<Vec<Data>, ParseError> {
+    fn param_list(input : &mut Chars) -> Result<Vec<Data>, ParseError> {
         parse_list!(input => parse_l_paren, parse_data : Data, parse_r_paren)
     }
 
@@ -72,7 +72,7 @@ fn parse_cons<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
     })
 }
 
-fn parse_symbol<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse_symbol(input : &mut Chars) -> Result<Data, ParseError> {
     pat!(parse_colon: char => () = ':' => ());
     parser!(input => {
         _colon <= parse_colon;
@@ -81,14 +81,14 @@ fn parse_symbol<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
     })
 }
 
-fn parse_string_data<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse_string_data(input : &mut Chars) -> Result<Data, ParseError> {
     parser!(input => {
         string <= parse_string;
         select Data::String(string)
     })
 }
 
-fn parse_list<'a>(input : &mut Chars<'a>) -> Result<Data, ParseError> {
+fn parse_list(input : &mut Chars) -> Result<Data, ParseError> {
     pat!(parse_l_square: char => () = '[' => ());
     pat!(parse_r_square: char => () = ']' => ());
 
