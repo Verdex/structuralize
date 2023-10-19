a492e3b2b1f6badf8cd2f29d086c058413e4e435
David L. Inman <davidlevere@gmail.com>
2023-10-18 23:34:19 -0400

    todo

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -4,6 +4,6 @@ mod data;
 pub mod parse;
 pub mod check;
 
-pub use data::*;
+pub use data::*; // TODO probably don't need to export this after patternable exists (but it should be public)
 
 pub mod matcher;
