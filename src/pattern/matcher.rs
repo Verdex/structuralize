600e659b2779e690645d41f8783b89d8c117ad4a
David L. Inman <davidlevere@gmail.com>
2023-10-25 23:40:39 -0400

    incremental progress for getting matchable and patternable implemented

--- src/pattern/matcher.rs
+++ src/pattern/matcher.rs
@@ -15,10 +15,10 @@ use super::check::*;
 //        should list return a vec or more likely a iter?  
 
 
-// TODO move this to patter::data here in a bit
-pub type MatchMap<'a> = Vec<(Box<str>, &'a Data)>; 
+// TODO move this to pattern::data here in a bit
+pub type MatchMap<'a, T> = Vec<(Box<str>, &'a T)>; 
 
-pub fn pattern_match<'data>(pattern : &TypeChecked, data : &'data Data) -> Matches<'data> {
+pub fn pattern_match<'a, D : Matchable>(pattern : &TypeChecked, data : &'a D) -> Matches<'a, D> {
     let p = pattern.pattern().clone();
     let mut current_work = Work::new();
     current_work.push((p, data));
@@ -26,14 +26,23 @@ pub fn pattern_match<'data>(pattern : &TypeChecked, data : &'data Data) -> Match
     Matches { matches: vec![], current_work, future_work: vec![] }
 }
 
-#[derive(Debug, Clone)]
-struct WorkPath<'a> {
-    work : Vec<(Pattern, &'a Data)>,
+#[derive(Debug)]
+struct WorkPath<'a, D : Matchable> {
+    work : Vec<(Pattern, &'a D)>,
     path : Vec<Pattern>,
-    nexts : Vec<&'a Data>,
+    nexts : Vec<&'a D>,
 }
 
-impl<'a> WorkPath<'a> {
+impl<'a, D : Matchable> Clone for WorkPath<'a, D> {
+    fn clone(&self) -> Self {
+        WorkPath { work: self.work.clone()
+                 , path: self.path.clone()
+                 , nexts: self.nexts.clone()
+                 }
+    }
+}
+
+impl<'a, D : Matchable> WorkPath<'a, D> {
     pub fn empty() -> Self {
         WorkPath { work: vec![], path: vec![], nexts: vec![] }
     }
@@ -42,11 +51,11 @@ impl<'a> WorkPath<'a> {
         WorkPath { work: vec![], path, nexts: vec![] }
     }
 
-    pub fn push(&mut self, item : (Pattern, &'a Data)) {
+    pub fn push(&mut self, item : (Pattern, &'a D)) {
         self.work.push(item);
     }
 
-    pub fn pop(&mut self) -> Option<(Pattern, &'a Data)> {
+    pub fn pop(&mut self) -> Option<(Pattern, &'a D)> {
         self.work.pop()
     }
 
@@ -55,17 +64,23 @@ impl<'a> WorkPath<'a> {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Work<'a> {
-    work : Vec<WorkPath<'a>>,
+#[derive(Debug)]
+struct Work<'a, D : Matchable> {
+    work : Vec<WorkPath<'a, D>>,
 }
 
-impl<'a> Work<'a> {
+impl<'a, D : Matchable> Clone for Work<'a, D> {
+    fn clone(&self) -> Self {
+        Work { work : self.work.clone() }
+    }
+}
+
+impl<'a, D : Matchable> Work<'a, D> {
     pub fn new() -> Self {
         Work { work : vec![WorkPath::empty()] }
     }
 
-    pub fn push(&mut self, item : (Pattern, &'a Data)) {
+    pub fn push(&mut self, item : (Pattern, &'a D)) {
         self.work.last_mut().unwrap().push(item);
     }
 
@@ -73,23 +88,23 @@ impl<'a> Work<'a> {
         self.work.len() == 1 && self.work.last().unwrap().work_finished()
     }
 
-    pub fn path(&mut self, mut patterns : Vec<Pattern>, data : &'a Data) {
+    pub fn path(&mut self, mut patterns : Vec<Pattern>, data : &'a D) {
         patterns.reverse();
         let first_pattern = patterns.pop().unwrap();
         self.work.push(WorkPath::new(patterns));
         self.push((first_pattern, data));
     }
 
-    pub fn next(&mut self, data : &'a Data) {
+    pub fn next(&mut self, data : &'a D) {
         self.work.last_mut().unwrap().nexts.push(data);
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Matches<'a> {
-    matches : MatchMap<'a>,
-    current_work : Work<'a>,
-    future_work : Vec<(MatchMap<'a>, Work<'a>)>,
+pub struct Matches<'a, D : Matchable> {
+    matches : MatchMap<'a, D>,
+    current_work : Work<'a, D>,
+    future_work : Vec<(MatchMap<'a, D>, Work<'a, D>)>,
 }
 
 // QueueWork
@@ -101,8 +116,8 @@ macro_rules! qw {
     };
 }
 
-impl<'a> Matches<'a> {
-    fn pop_current_work(&mut self) -> Option<(Pattern, &'a Data)> { 
+impl<'a, D : Matchable> Matches<'a, D> {
+    fn pop_current_work(&mut self) -> Option<(Pattern, &'a D)> { 
         if let Some(ret) = self.current_work.work.last_mut().unwrap().pop() {
             Some(ret)
         }
@@ -132,30 +147,31 @@ impl<'a> Matches<'a> {
     }
 }
 
-impl<'a> Iterator for Matches<'a> {
-    type Item = MatchMap<'a>;
+impl<'a, D : Matchable> Iterator for Matches<'a, D> {
+    type Item = MatchMap<'a, D>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.current_work.work_finished() {
             return None;
         }
 
-        while let Some(w) = self.pop_current_work() {
-            match w {
-                (Pattern::CaptureVar(name), data) => { self.matches.push((name.into(), data)); },
-                (Pattern::ExactList(ps), Data::List(ds)) if ps.len() == 0 && ds.len() == 0 => { /* pass */ },
-                (Pattern::ExactList(ps), Data::List(ds)) if ps.len() == ds.len() => {
+        while let Some((pattern, matchable)) = self.pop_current_work() {
+            match (pattern, matchable.kind()) {
+                (Pattern::CaptureVar(name), _) => { self.matches.push((name.into(), matchable)); },
+                (Pattern::ExactList(ps), MatchKind::List(ds)) if ps.len() == 0 && ds.len() == 0 => { /* pass */ },
+                (Pattern::ExactList(ps), MatchKind::List(ds)) if ps.len() == ds.len() => {
                     qw!(self.current_work, ps, ds);
                 },
 
-                (Pattern::Cons {name: pname, params: pparams}, Data::Cons {name: dname, params: dparams}) 
-                    if pname == *dname && pparams.len() == dparams.len() => {
+                (Pattern::Cons {name: pname, params: pparams}, MatchKind::Cons(dname, dparams)) 
+                // TODO !!!! pname is hopefully becoming &str soon ::: REMOVE .into() from dname
+                    if pname == dname.into() && pparams.len() == dparams.len() => {
 
                     qw!(self.current_work, pparams, dparams);
                 },
 
-                (Pattern::ListPath(ps), Data::List(_)) if ps.len() == 0 => { /* pass */ },
-                (Pattern::ListPath(ps), Data::List(ds)) if ps.len() <= ds.len() => {
+                (Pattern::ListPath(ps), MatchKind::List(_)) if ps.len() == 0 => { /* pass */ },
+                (Pattern::ListPath(ps), MatchKind::List(ds)) if ps.len() <= ds.len() => {
                     let p_len = ps.len();
 
                     for i in (1..=(ds.len() - p_len)).rev() { // TODO I would be really great if this were lazy
@@ -170,34 +186,34 @@ impl<'a> Iterator for Matches<'a> {
                 },
 
                 (Pattern::Wild, _) => { /* pass */ },
-                (Pattern::Symbol(p), Data::Symbol(d)) if p == *d => { /* pass */ }, 
-                (Pattern::String(p), Data::String(d)) if p == *d => { /* pass */ },
+                //(Pattern::Symbol(p), Data::Symbol(d)) if p == *d => { /* pass */ }, 
+                //(Pattern::String(p), Data::String(d)) if p == *d => { /* pass */ },
 
-                (Pattern::TemplateVar(var), data) => {
+                (Pattern::TemplateVar(var), _) => {
                     let (_, d) = self.matches.iter().find(|(k, _)| k == &var ).unwrap();
-                    let p = data_to_pattern(d);
-                    self.current_work.push((p, data));
+                    let p = data_to_pattern(*d);
+                    self.current_work.push((p, matchable));
                 },
 
-                (Pattern::And(a, b), data) => {
-                    self.current_work.push((*b, data));
-                    self.current_work.push((*a, data));
+                (Pattern::And(a, b), _) => {
+                    self.current_work.push((*b, matchable));
+                    self.current_work.push((*a, matchable));
                 },
 
-                (Pattern::PathNext, data) => { 
-                    self.current_work.next(data);
+                (Pattern::PathNext, _) => { 
+                    self.current_work.next(matchable);
                 },
                 
                 (Pattern::Path(ps), _) if ps.len() == 0 => { /* pass */ },
-                (Pattern::Path(ps), data) => {
-                    self.current_work.path(ps.clone(), data);   
+                (Pattern::Path(ps), _) => {
+                    self.current_work.path(ps.clone(), matchable);   
                 },
 
-                (Pattern::Or(a, b), data) => {
+                (Pattern::Or(a, b), _) => {
                     let mut work = self.current_work.clone();
-                    work.push((*b, data));
+                    work.push((*b, matchable));
                     self.future_work.push((self.matches.clone(), work));
-                    self.current_work.push((*a, data));
+                    self.current_work.push((*a, matchable));
                 },
 
                 _ => { 
@@ -227,11 +243,15 @@ impl<'a> Iterator for Matches<'a> {
     }
 }
 
-fn data_to_pattern(data : &Data) -> Pattern {
-    match data {
-        Data::String(s) => Pattern::String(s.clone()), 
-        Data::Symbol(s) => Pattern::Symbol(s.clone()),
-        Data::Cons { name, params } => Pattern::Cons { name: name.clone(), params: params.iter().map(data_to_pattern).collect() },
-        Data::List(ds) => Pattern::ExactList(ds.iter().map(data_to_pattern).collect()),
+// TODO: rename to something like matchable_to_pattern
+// TODO:  an Atom matchable probably results in a pattern that matches fine, but an object probably does not and that might be weird to consumers
+fn data_to_pattern<D : Matchable>(data : &D) -> Pattern {
+    match data.kind() {
+        //Data::String(s) => Pattern::String(s.clone()), 
+        //Data::Symbol(s) => Pattern::Symbol(s.clone()),
+        // TODO name.clone().into() needs fixed !!!
+        MatchKind::Cons(name, params) => Pattern::Cons { name: name.clone().into(), params: params.iter().map(data_to_pattern).collect() },
+        MatchKind::List(ds) => Pattern::ExactList(ds.iter().map(data_to_pattern).collect()),
+        _ => todo!(),
     }
 }
